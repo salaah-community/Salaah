226cd157670d58943c180f716ac0c7701e9d9a11
V1V3K09 <113044699+V1V3K09@users.noreply.github.com>
2023-09-24 00:49:12 +0530

    2023 team update

--- src/pages/Team/TeamData/Leads.js
+++ src/pages/Team/TeamData/Leads.js
@@ -1,97 +1,83 @@
-import aryan from '../../../assets/team/aryanporwal.jpg'
-import darshika from '../../../assets/team/darshika.jpeg'
-import abhishekY from '../../../assets/team/abhishek.jpg'
-import poras from '../../../assets/team/poras.jpeg'
-import anshika from '../../../assets/team/anshikach.JPG'
-import paridhi from '../../../assets/team/paridhi.jpeg'
-import shoaib from '../../../assets/team/shoaib.jpg'
-import ashish from '../../../assets/team/ashish.png'
-import  yash from '../../../assets/team/sarin.jpeg'
-import amar from '../../../assets/team/amar.jpg'
-import bhoomi from '../../../assets/team/bhoomi.jpg'
-import saurabh from '../../../assets/team/saurabhsrivastava.jpg'
-import sahil from '../../../assets/team/sahilgupta.jpeg'
-import vanshika from '../../../assets/team/vanshika.jpg'
-import akash from '../../../assets/team/akash.jpeg'
-
+import vivek from "../../../assets/team/vivek.jpeg";
+import ish from "../../../assets/team/ish.jpg";
+import khushi from "../../../assets/team/kushi.jpeg";
+import mishita from "../../../assets/team/Mishita_Saxena.jpg";
+import faraz from "../../../assets/team/Faraz Hafeez.jpg";
+import tanya from "../../../assets/team/Tanya.jpg";
+import abhishek from "../../../assets/team/Abhishek Yadav.jpg";
+import aryan from "../../../assets/team/aryan garg.jpeg";
+import sakshi from "../../../assets/team/sakshi.jpg";
+import harsh from "../../../assets/team/Harsh Jadoun.jpg";
+import vrihsa from "../../../assets/team/vrisha.jpg";
+import divyansh from "../../../assets/team/Divyansh Agarwal.jpeg";
 
 
 
 
 export const teamLeads = [
+    // Head of Technical Operations
     {        
-        name: 'Mr. Aryan Porwal',
-        title: 'Oration Head',
+        name: 'Vivek Kumar Tiwari',
+        title: 'Head of Technical Operations',
+        img: vivek 
+    },
+    {        
+        name: 'Ish Purwar',
+        title: 'Head of Technical Operations',
+        img: ish
+    },
+    // Head of Resource Management & Activities
+    {        
+        name: 'Khushi Chuadhary',
+        title: 'Head of Resource Management & Activities',
+        img: khushi
+    },
+{        
+        name: 'Mishita Saxena',
+        title: 'Head of Resource Management & Activities',
+        img: mishita
+    },
+{        
+        name: 'Faraz Hafeez',
+        title: 'Head of Resource Management & Activities',
+        img: faraz
+    },
+    //Head of Social Media & Marketing
+{        
+        name: 'Tanya Srivastava',
+        title: 'Head of Social Media & Marketing',
+        img: tanya
+    },
+{        
+        name: 'Abhishek Yadav',
+        title: 'Head of Social Media & Marketing',
+        img: abhishek
+    },
+{        
+        name: 'Aryan Garg',
+        title: 'Head of Social Media & Marketing',
         img: aryan
     },
-    {        
-        name: 'Ms. Darshika Gupta',
-        title: 'Oration Head',
-        img: darshika
-    },
-    {        
-        name: 'Mr. Abhishek Yadav',
-        title: 'Planning & Strategy Head',
-        img: abhishekY
+    //Head of Mentor-Mentee & Outreach
+{        
+        name: 'Sakshi Sharma',
+        title: 'Head of Mentor-Mentee & Outreach',
+        img: sakshi
     },
 {        
-        name: 'Mr. Poras Saini',
-        title: 'Planning & Strategy Head',
-        img: poras
+        name: 'Harsh Jadoun',
+        title: 'Head of Mentor-Mentee & Outreach',
+        img: harsh
+    },
+    //Head of Mock Interview & Technical Program
+{        
+        name: 'Vrisha Garg',
+        title: 'Head of Mock Interview & Technical Program',
+        img: vrihsa
     },
 {        
-        name: 'Ms. Anshika Chauhan',
-        title: 'Content Head',
-        img: anshika
-    },
-{        
-        name: 'Ms. Paridhi Pandey',
-        title: 'Content Head',
-        img: paridhi
-    },
-{        
-        name: 'Mr. Shoaib Ahmed',
-        title: 'Outreach Head',
-        img: shoaib
-    },
-{        
-        name: 'Mr. Ashish Dwivedi',
-        title: 'Outreach Head',
-        img: ashish
-    },
-{        
-        name: 'Mr. Yash Sarin',
-        title: 'Outreach Head',
-        img: yash
-    },
-{        
-        name: 'Mr. Amar Jyoti',
-        title: 'Technical Head',
-        img: amar
-    },
-{        
-        name: 'Ms. Bhoomi',
-        title: 'Technical Head',
-        img: bhoomi
-    },
-{        
-        name: 'Mr. Saurabh Srivastava',
-        title: 'Technical Head',
-        img: saurabh
-    },
-{        
-        name: 'Mr. Sahil Gupta',
-        title: 'Technical Head',
-        img: sahil
-    },
-    {        
-        name: 'Ms. Vanshika Jain',
-        title: 'Content Head',
-        img: vanshika
-    },
-    {        
-        name: 'Mr. Akash Sharma',
-        title: 'Outreach Head',
-        img: akash
-    },
+        name: 'Divyansh Agarwal',
+        title: 'Head of Mock Interview & Technical Program',
+        img: divyansh
+    }
 ]
