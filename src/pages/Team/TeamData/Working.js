2a6172738b01f83e044bfeb769f3daa1229ae8b0
Rakesh1105-code <kundurakesh66@gmail.com>
2025-11-23 22:04:28 +0530

    Updated Working.js team data

--- src/pages/Team/TeamData/Working.js
+++ src/pages/Team/TeamData/Working.js
@@ -83,4 +83,161 @@ export const teamWorking = [
     img: "",
   },
 
+// New Working Committee 2025-26:
+
+{
+//Outreach:
+name:" Mr.Aksh Chauhan",
+title:"Outreach",
+img:"",
+},
+{
+name:"Ms.Avika Pal",
+title:"Outreach",
+img:"",
+},
+{
+name:"Ms.Riya Yadav",
+title:"Outreach",
+img:"",
+},
+{
+name:"Mr.Saurav Tyagi",
+title:"Outreach",
+img:"",
+},
+{
+name:"Ms.Tanushree Saxena",
+title:"Outreach",
+img:"",
+},
+{
+name:"Ms.Vanshika Garg",
+title:"Outreach",
+img:"",
+},
+//Social Media and Marketing:
+{
+name:"Mr.Ankur",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Ms.Anshita Varshney",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Mr.Aryan",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Mr.Ashish Gupta",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Mr.Ashish Kashyap",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Mr.Ramyak Verma",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Ms.Reshal Dhupar",
+title:"Social Media and Marketing",
+img:"",
+},
+{
+name:"Mr.Suraj",
+title:"Social Media and Marketing",
+img:"",
+},
+//Tecnical Opeartion:
+{
+  name:"Mr.Rakesh Kundu",
+  title:"Technical operation",
+  img:""
+},
+{
+  name:"Ms.Ekta Jamloki",
+  title:"Technical operation",
+  img:""
+},
+{
+  name:"Mr.Bhuwan Sharma",
+  title:"Technical operation",
+  img:""
+},
+{
+  name:"Ms.Soumya Baranwal",
+  title:"Technical operation",
+  img:""
+},
+//Content and Documentation:
+{
+  name:"Mr.Aayush Joshi",
+  title:"Content and Documentation",
+  img:""
+},
+{
+  name:"Ms.Anshika Choudhary",
+  title:"Content and Documentation",
+  img:""
+},
+{
+  name:"Mr.Anuj Pratap Singh",
+  title:"Content and Documentation",
+  img:""
+},
+{
+  name:"Ms.Khushi Saini",
+  title:"Content and Documentation",
+  img:""
+},
+{
+  name:"Mr.Shivang",
+  title:"Content and Documentation",
+  img:""
+},
+{
+  name:"Mr.Yashashkar Srivastava",
+  title:"Content and Documentation",
+  img:""
+},
+//Planning and Strategy:
+{
+  name:"Mr.Abhinav Pratap Singh",
+  title:"Planning and Strategy",
+  img:""
+},
+{
+  name:"Ms.Ananya Sharma",
+  title:"Planning and Strategy",
+  img:""
+},
+{
+  name:"Ms.Aparna Nigam",
+  title:"Planning and Strategy",
+  img:""
+},
+{
+  name:"Mr.Avinash Kumar Shukla",
+  title:"Planning and Strategy",
+  img:""
+},
+{
+  name:"Mr.Priyansh Gupta",
+  title:"Planning and Strategy",
+  img:""
+},
+{
+  name:"Mr.Rahul Raj Tiwari",
+  title:"Planning and Strategy",
+  img:""
+},
 ];
